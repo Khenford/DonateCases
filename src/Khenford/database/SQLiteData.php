6328ac260750c78eff46873b101ad77d6c34b9ca
Khenford <130295382+Khenford@users.noreply.github.com>
2024-02-07 23:36:56 +0800

    Create SQLiteData.php

---
+++ src/Khenford/database/SQLiteData.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Khenford\database;
+
+interface SQLiteData{  
+
+    public function transferKey(string $fromUser, string $toUser, int $key): void;   
+
+    public function addUserData(string $user, int $key): void;    
+
+    public function addKey(string $user, int $key): void;   
+
+    public function removeKey(string $user, int $key): void;    
+
+    public function isUser(string $user): bool;    
+
+    public function getKeys(string $user): int;
+}
